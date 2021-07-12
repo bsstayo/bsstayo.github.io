567b36132b5a100e72832a7f879abce4dd786841
김도훈 <k99hoon@gmail.com>
2021-07-12 13:08:05 +0000

    add some tutorials

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    postname = models.CharField(max_length=50)
+    contents = models.TextField()
+    
+    def __str__(self):
+        return self.postname
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+# 모델에 Post 가져오기
+from .models import Post
 
+# Create your views here.
 def index(request):
     return render(request, 'main/index.html')
 
+# blog에 Post를 전부 가져오자
 def blog(request):
-    return render(request, 'main/blog.html')
+    postlist = Post.objects.all()
+    return render(request, 'main/blog.html', {'postlist':postlist})
+
+# 게시글별 세부페이지
+def postdetails(request, pk):
+    postlist = Post.objects.get(pk=pk)
+    return render(request, 'main/postdetails.html', {'postlist':postlist})
