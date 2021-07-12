8f8cd8770d84b762bb5dceedb2dbc47199e982cf
Do-Hoon Kim <k99hoon@gmail.com>
2021-07-12 22:12:44 +0900

    Delete mysite directory
    Not suitable for github pages

--- mysite/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    postname = models.CharField(max_length=50)
-    contents = models.TextField()
-    
-    def __str__(self):
-        return self.postname
