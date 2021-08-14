bbd4126288f646c07d68ac182fc5a0f5a422936a
김도훈 <k99hoon@gmail.com>
2021-08-14 13:48:44 +0000

    separate script and html

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function openRightMenu() {         
+    document.getElementById("rightMenu").style.display = "block";
+}
+                
+function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+}
