7d0bbeb83d98dafeae9ee482dc08cf310fdc3d7b
김도훈 <k99hoon@gmail.com>
2021-08-14 14:40:53 +0000

    test code for about info background

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,18 @@
 function openRightMenu() {         
     document.getElementById("rightMenu").style.display = "block";
+    //document.getAnimations("rightMenu").slideToggle();
+    //document.getElementById("rightMenu").slideToggle();
 }
                 
 function closeRightMenu() {
     document.getElementById("rightMenu").style.display = "none";
+    //document.getAnimations("rightMenu").slideToggle();
+}
+
+function about() {
+    closeRightMenu();
+    var box = document.getElementById("about");
+    box.style.display = "block";
+    box.style.backgroundColor = "rgba(0,0,0,0.85)";
+    box.style.zIndex = "1";
 }
