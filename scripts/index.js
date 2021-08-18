6efeff4a32b439161e8ff6f31f25a922348ec867
김도훈 <k99hoon@gmail.com>
2021-08-18 14:34:09 +0000

    add codes for about screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,21 @@
 function about() {
-    document.getElementById("mySidebar").style.display = "none";
-    //document.getElementById("myOverlay").style.display = "block";
-    document.getElementById("aboutOverlay").style.display = "block";
-    document.getElementById("aboutClose").style.display = "block";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+  document.getElementById("aboutOverlay").style.display = "block";
+  //document.getElementById("aboutClose").style.display = "block";
+}
+
+function closeOverlay() {
+  document.getElementById("aboutOverlay").style.display = "none";
+  //document.getElementById("aboutClose").style.display = "none";
 }
 
 function openRightMenu() {
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("myOverlay").style.display = "block";
-  //document.getElementById("mySidebar").classList.toggle('w3-animate-right');
-  //document.getElementById("mySidebar").classList.toggle('w3-animate-left');
 }
 
 function closeRightMenu() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("myOverlay").style.display = "none";
-  //document.getElementById("mySidebar").classList.toggle('w3-animate-right');
-  //document.getElementById("mySidebar").classList.toggle('w3-animate-left');
 }
